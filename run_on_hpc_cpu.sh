a031df9a6bdfb4e066f3f294dda9c881e9a21457
PDang10001 <60625154+ptdang1001@users.noreply.github.com>
2025-04-11 14:37:40 -0700

    Update run_on_hpc_cpu.sh

--- run_on_hpc_cpu.sh
+++ run_on_hpc_cpu.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1 # Number of tasks
 #SBATCH --cpus-per-task=8 # Number of CPU cores per task (adjust as needed)
 #SBATCH --mem=128G                     # Request xGB of memory
-#SBATCH --time=02:00:00               # Set a x-hour time limit
+#SBATCH --time=04:00:00               # Set a x-hour time limit
 
 
 # Read the input arguments
@@ -16,7 +16,7 @@ gene_expression_file_name=$4
 compounds_reactions_file_name=$5
 reactions_genes_file_name=$6
 
-python3 src/main.py \
+uv run src/main.py \
     --input_dir_path ${input_dir_path} \
     --network_dir_path ${network_dir_path} \
     --output_dir_path ${output_dir_path} \
