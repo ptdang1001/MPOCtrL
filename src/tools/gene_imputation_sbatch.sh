242c915a74cf0511b27d0691d363cabe60648a1f
PDang10001 <60625154+ptdang1001@users.noreply.github.com>
2025-04-10 19:31:21 -0700

    Create gene_imputation_sbatch.sh

---
+++ src/tools/gene_imputation_sbatch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+#SBATCH --job-name=gene_imputation            # Job name
+#SBATCH --partition=batch               # Use the GPU partition (modify as needed)
+#SBATCH --ntasks=1 # Number of tasks
+#SBATCH --cpus-per-task=8 # Number of CPU cores per task (adjust as needed)
+#SBATCH --mem=32G                     # Request xGB of memory
+#SBATCH --time=8:00:00               # Set a x-hour time limit
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=xxxxxxx@xxxx.com # your email address
+#SBATCH -o ./%j_stdOutput.txt # Standard output
+#SBATCH -e ./%j_stdError.txt # Standard error
+
+# Read the input arguments
+DATA_DIR="/path/to/data/"       # Replace with your data directory
+DATA_NAME="dataset_name.csv.gz" # Replace with your dataset name
+
+python gene_imputation.py \
+    --data_dir ${DATA_DIR} \
+    --data_name ${DATA_NAME}
